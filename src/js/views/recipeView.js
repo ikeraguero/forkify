6cdf01229bbaac7e2d2d853415bafe35d7699a2e
ikeraguero <ikerpires407@gmail.com>
2024-06-13 20:06:51 -0300

    Add Fractional Quantities

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,6 @@
 import { mark } from 'regenerator-runtime';
 import icons from '../../img/icons.svg'
+import { Fraction } from 'fractional';
 
 class RecipeView {
     #parentEl = document.querySelector(".recipe");
@@ -78,7 +79,7 @@ class RecipeView {
         <svg class="recipe__icon">
           <use href="${icons}#icon-check"></use>
         </svg>
-        <div class="recipe__quantity">${ing.quantity ? ing.quantity : ''}</div>
+        <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
         <div class="recipe__description">
           <span class="recipe__unit">${ing.unit}</span>
           ${ing.description}
